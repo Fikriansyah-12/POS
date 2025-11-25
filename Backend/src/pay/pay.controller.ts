ead2fda909b33f69ccf99fc3d98b26d75f0908cc
fikri-ans <widiantoaris170@gmail.com>
2025-11-25 15:06:35 +0700

    add/midtrans

--- Backend/src/pay/pay.controller.ts
+++ Backend/src/pay/pay.controller.ts
@@ -1,4 +1,51 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import { PaymentStatus } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Controller('pay')
-export class PayController {}
+export class PayController {
+  constructor(private prisma: PrismaService) {}
+
+  @Post('webhook')
+  @HttpCode(200)
+  async handlewebHook(@Body() body: any) {
+    const { order_id, transaction_status, transaction_id, payment_type } = body;
+
+    const sale = await this.prisma.sale.findFirst({
+      where: {
+        midtransOrderId: order_id,
+      },
+    });
+
+    if (!sale) {
+      return { status: 'ok' };
+    }
+
+    let statusPembayaran: PaymentStatus = PaymentStatus.UNPAID;
+
+    if (transaction_status === 'settlement') {
+      statusPembayaran = PaymentStatus.PAID;
+    } else if (
+      transaction_status === 'cancel' ||
+      transaction_status === 'expire' ||
+      transaction_status === 'deny'
+    ) {
+      statusPembayaran = PaymentStatus.CANCELLED;
+    }
+     await this.prisma.sale.update({
+      where: { id: sale.id },
+      data: {
+        midtransStatus: transaction_status,
+        midtransTransactionId: transaction_id,
+        midtransPaymentType: payment_type,
+        statusPembayaran,
+        jumlah_bayar:
+          transaction_status === 'settlement'
+            ? sale.total_bersih
+            : sale.jumlah_bayar,
+      },
+    });
+
+    return { status: 'ok' };
+  }
+}
--- Backend/src/pay/pay.service.ts
+++ Backend/src/pay/pay.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
 
+const midtransClient = require('midtrans-client');
+
 @Injectable()
-export class PayService {}
+export class PayService {
+  private snap: any
+
+  constructor(){
+    this.snap = new midtransClient.Snap({
+      isProduction: process.env.MIDTRANS_IS_PRODUCTION === 'false',
+      serverKey: process.env.MIDTRANS_SERVER_KEY,
+      clientKey: process.env.MIDTRANS_CLIENT_KEY,
+    });
+  }
+  async createTransaction(payload: any){
+    return this.snap.createTransaction(payload)
+  }
+}
